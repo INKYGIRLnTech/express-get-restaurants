d6e40caa8da936609b0f59a234a2d6c827102b3f
Meagan Hollingsworth <mhollingsworth@google.com>
2023-10-16 18:23:34 -0400

    created express routes

--- src/app.js
+++ src/app.js
@@ -10,6 +10,62 @@ app.get("/restaurants", async (req, res) => {
     res.json(restaurants);
 })
 
+app.get("/restaurants/:id", async (req, res) => {
+    const number = req.params.id;
+    const restaurants = await Restaurant.findByPk(number);
+    res.json(restaurants);
+})
+
+app.use(express.json());
+app.use(express.urlendcoded());
+
+
+app.get('/restaurants/:id', async(req, res, next)=>{
+    try{
+        const id = req.params.id;
+        const restaurants = await Restaurant.findByPk(id);
+        res.json(restaurants);
+    }catch(err){
+        next(err)
+    }
+})
+
+app.put('/restaurants/:id', async(req, res, next)=>{
+    try{
+        const restaurants = await Restaurant.findByPk(req.params.id)
+        const updated = await restaurants.update(req.body)
+        console.log(updated);
+        res.json(updated);
+    }
+    catch(err){
+        next(err);
+    }
+})
+
+app.put('/restaurants/:id', async(req, res, next)=>{
+    try{
+        const restaurants = await Restaurant.findByPk(req.params.id)
+        const updated = await restaurants.update(req.body)
+        console.log(updated);
+        res.json(updated);
+    }
+    catch(err){
+        next(err);
+    }
+})
+
+app.delete('/restaurants/:id', async (req, res, next)=>{
+    try{
+     
+     await Restaurant.destroy({
+         where: {id: req.params.id}});
+     res.sendStatus(200);
+    }
+    catch(err){
+     next(err);
+    }
+     
+ })
 
 
 
